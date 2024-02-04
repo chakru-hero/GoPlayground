27625ce5ae34764379448c0ae8bf7c4a8c4c0d5e
Chakradhar Dwivedi <chakradhar.dwivedi2000@gmail.com>
2024-02-05 03:41:46 +0530

    Renamed file

---
+++ Playground1/linkedlists/deep_copy_ll.go
@@ -0,0 +1,27 @@
+package linkedlists
+
+
+//type RandomListNode struct {
+//  value int
+//  next * RandomListNode
+//  random * RandomListNode
+//}
+//func newRandomListNode(value int) *RandomListNode {
+//    node := new(RandomListNode);
+//   node.value = value;
+//    node.next = nil;
+//    node.random = nil;
+//    return node;
+//}
+//func copyRandomList(head *RandomListNode) *RandomListNode {
+//    temp := head;
+//    newHead := newRandomListNode(head.value);
+//    newTemp := newHead;
+//
+//    for temp !=nil {
+//        temp = temp.next;
+//        newTemp.next = newRandomListNode(temp.value) ;
+//        newTemp = newTemp.next;
+//    }
+//    return newHead;
+//}
